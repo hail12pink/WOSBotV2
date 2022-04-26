12364c905a5225dd731535f46aa502af3638eeec
hail12pink <hail12pink@users.noreply.github.com>
2022-04-26 11:39:06 -0400

    basically finished

--- src/commands/part.js
+++ src/commands/part.js
@@ -1,17 +1,17 @@
-const { MessageEmbed, Message } = require("discord.js")
+const { MessageEmbed } = require("discord.js")
 const fetch = require("node-fetch")
 
 function componentToHex(c) { // https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
     var hex = c.toString(16);
     return hex.length == 1 ? "0" + hex : hex;
-  }
+}
 
 function rgbToHex(rgb) { // https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
     return "#" + componentToHex(rgb[0]) + componentToHex(rgb[1]) + componentToHex(rgb[2]);
 }
 
 module.exports = {
-    name: "getpart",
+    name: "part",
     description: "gets information about a waste of space part",
     cooldown: 1,
     permissions: [],
@@ -21,10 +21,16 @@ module.exports = {
             "description": "The part's name.",
             "type": 3,
             "required": true
+        },
+        {
+            "name": "raw_json",
+            "description": "whether to send json instead",
+            "type": 5,
+            "required": false
         }
     ],
 
-    run: async(client, interaction) => {
+    run: async (client, interaction) => {
         const parts = await (await fetch("http://12pink.dev/data/parts.json")).json()
         const partName = interaction.options.getString("part")
         const user = interaction.user
@@ -33,11 +39,25 @@ module.exports = {
         if (part) {
             console.log(interaction.user)
 
-            const embed = new MessageEmbed
-            embed.setColor(rgbToHex(part.DefaultColor))
-            embed.setTitle(partName)
-            embed.setDescription(part.Description || "[ No description entry ]")
-            embed.setFooter({ text: `${user.username}#${user.discriminator}`, iconURL: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png` })
+            if (interaction.options.getBoolean("raw_json") == true) {
+                return interaction.reply("raw json:\n```js\n" + JSON.stringify(part) + "\n```")
+            }
+
+            const embed = new MessageEmbed()
+            .setColor(rgbToHex(part.DefaultColor))
+            .setTitle(partName)
+            .setDescription(part.Description || "[ No description entry ]")
+            .setFooter({ text: `${user.username}#${user.discriminator}`, iconURL: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png` })
+            .addFields(
+                { name: "Durability", value: (part.Durability || 3).toString(), inline: true },
+                { name: "State", value: (part.ClassState || 3).toString(), inline: true },
+            )
+
+            if (part.Flammable) {
+                embed.addField("Flammable", ":white_check_mark:", true)
+            } else {
+                embed.addField("Flammable", ":x:", true)
+            }
 
             if (part.Recipe) {
                 let finished = ""
@@ -47,22 +67,18 @@ module.exports = {
                     finished += `${item[0]}: ${item[1]}\n`
                 })
 
-                embed.addField("Recipe", finished.substring(-2))
+                embed.addField("Recipe", finished.substring(-2), true)
             } else {
-                embed.addField("Recipe", "No crafting recipe.")
+                embed.addField("Recipe", "No crafting recipe.", true)
             }
 
             if (part.Malleabiltity && part.Malleability > 0) {
-                embed.addField("Malleability", part.Malleability.toString())
+                embed.addField("Malleability", part.Malleability.toString(), true)
             } else {
                 const size = part.DefaultSize
 
-                embed.addField("Malleability", `Fixed size (${size[0] || "?"}, ${size[1] || "?"}, ${size[2] || "?"})`)
+                embed.addField("Malleability", `Fixed size (${size[0] || "?"}, ${size[1] || "?"}, ${size[2] || "?"})`, true)
             }
-
-            embed.addField("State", part.ClassState)
-
-            embed.addField("Flammable", String(part.Flammable || false))
 
             if (part.ConfigData) {
                 for (const [i, data] of Object.entries(part.ConfigData)) {
@@ -73,15 +89,19 @@ module.exports = {
                     const description = data[4];
 
                     if (type == "string") {
-                        embed.addField(name, `${description}\nstring \ndefault: "**${defaultValue}**"`)
-                    } else if (type == "ResourceString") {
-                        embed.addField(name, `${description}\nstring or URL\ndefault: "**${defaultValue}**"`)
+                        embed.addField(name, `${description}\n(string) default: "**${defaultValue}**"`)
                     } else if (type == "boolean") {
-                        embed.addField(name, `${description}\nboolean\ndefault: **${defaultValue || false}**`)
+                        embed.addField(name, `${description}\n(boolean) default: **${defaultValue || false}**`)
                     } else if (type == "number") {
-                        embed.addField(name, `${description}\nnumber\ndefault: **${defaultValue| "?"}**`)
+                        embed.addField(name, `${description}\n(number) default: **${defaultValue | "?"}**\nrange: ${range[0]} - ${range[1]}`)
                     } else if (type == "Coordinate") {
-                        embed.addField(name, `${description}\nCoordinates\ndefault: **${defaultValue}**`)
+                        embed.addField(name, `${description}\n(Coordinates) default: **${defaultValue}**`)
+                    } else if (type == "ResourceString") {
+                        embed.addField(name, `${description}\n(string or URL) default: "**${defaultValue}**"`)
+                    } else if (type == "MaterialToExtract") {
+                        embed.addField(name, `${description}\n(MaterialToExtract) default: **${defaultValue}**`)
+                    } else if (type == "Selection") {
+                        embed.addField(name, `${description}\n(Selection) default: **${defaultValue}**\noptions:\n${range.join("\n")}`)
                     }
                 }
             }
