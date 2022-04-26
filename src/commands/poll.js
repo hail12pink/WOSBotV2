2bd09d17b631b694dd4595415239efd28e0b352d
hail12pink <hail12pink@users.noreply.github.com>
2022-04-26 12:22:51 -0400

    created poll.js

---
+++ src/commands/poll.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "poll",
+    description: "Create a poll",
+    cooldown: 1,
+    permissions: [],
+    options: [],
+
+    run: async(client, interaction) => {
+        // code here
+    }
+}
