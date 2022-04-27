bac27307afa6f0d9b97c96c0f71c6e4a962a04b6
hail12pink <hail12pink@users.noreply.github.com>
2022-04-27 08:57:21 -0400

    added memory usage

--- src/commands/botinfo.js
+++ src/commands/botinfo.js
@@ -1,5 +1,6 @@
 const { MessageEmbed } = require("discord.js")
 const os = require("os")
+const process = require("process")
 
 module.exports = {
     name: "botinfo",
@@ -11,12 +12,21 @@ module.exports = {
     run: async(client, interaction) => {
         const uptime = client.readyTime //Date.now() - client.readyTime
         console.log(uptime)
+
+        console.log(os.totalmem())
+
+        const totalmem = Math.round((os.totalmem()/1000/1000*100)/1000)
+        const usedmem = Math.round((process.memoryUsage().heapUsed/1000/1000*100)/1000)
+
+        console.log(usedmem)
+        console.log(process.memoryUsage())
         
         const embed = new MessageEmbed()
 
             .addFields(
                 { name: "Operating System", value: `${os.type()} (${os.arch()})`, inline: true },
-                { name: "Uptime", value: `Since <t:${Math.floor(uptime/1000)}:R>` }
+                { name: "Uptime", value: `Since <t:${Math.floor(uptime/1000)}:R>` },
+                { name: "Memory Usage", value: `${usedmem*10}/${totalmem*10}MB (${Math.round((usedmem/totalmem)*100)/100}%)`, inline: true }
             )
 
         interaction.reply({ embeds: [embed] })
