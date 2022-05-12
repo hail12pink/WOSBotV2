5604565400d049c1cc550b02900889b4cbe9c800
hail12pink <hail12pink@users.noreply.github.com>
2022-05-12 09:42:10 -0400

    command coded

--- src/commands/getserial.js
+++ src/commands/getserial.js
@@ -17,6 +17,28 @@ module.exports = {
     ],
 
     run: async (client, interaction) => {
-        console.log("not even real yet")
+        const serial = interaction.options.getString("serial")
+        const shipData = client.serials[serial]
+
+        if (shipData) {
+            const embed = new MessageEmbed()
+            .setColor("GREEN")
+            .setTitle(serial)
+            .addFields(
+                { name: "Owner", value: shipData.Owner, inline: true },
+                { name: "Status", value: shipData.Status, inline: true },
+                { name: "Date-Of-Activation", value: `<t:${Math.floor(shipData.DOA/1000)}:R>`, inline: true },
+                { name: "Assigned script", value: shipData.Script, inline: true }
+            )
+
+            interaction.reply({ embeds: [embed] })
+        } else {
+            const embed = new MessageEmbed()
+            .setColor("RED")
+            .setTitle("Invalid serial")
+            .setDescription("The specified serial is not valid.")
+
+            return interaction.reply({ embeds: [embed], ephemeral: true })
+        }
     }
 }
