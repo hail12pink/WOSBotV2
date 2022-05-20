cebe7c07e323779af57bf35658200a57fc69c756
hail12pink <hail12pink@users.noreply.github.com>
2022-05-20 12:35:45 -0400

    purged sensitive information

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+const port = 2069
+
+const fetch = require("node-fetch")
+
+const { Client, Collection, Intents } = require("discord.js")
+const { readdirSync, writeFileSync, readFileSync } = require("fs")
+const logs = require("./src/commands/logs")
+const client = new Client({
+    allowedMentions: { parse: ['users', 'roles'] },
+    fetchAllMembers: false,
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS]
+})
+
+client.commands = new Collection()
+client.config = require("./config.json")
+client.serials = require("./serials.json")
+client.universe = require("./universe.json")
+client.logs = []
+cooldowns = new Collection()
+
+// load handlers
+readdirSync("./src/utils/handlers/").map(async (handler) => {
+    require(`./src/utils/handlers/${handler}`)(client)
+
+    console.log(`loaded handler '${handler}'`)
+})
+
+app.listen(port, () => {
+    console.log(`server listening at localhost:${port}`)
+})
+
+client.login(client.config.token)
